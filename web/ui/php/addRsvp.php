17c0cea51fbdb921aca4d705c8782a960813aea1
Dandre Allison <dandre.allison@gmail.com>
2012-01-30 20:01:13 -0800

    Merging in the work down on the server

--- web/ui/php/addRsvp.php
+++
@@ -1,9 +0,0 @@
-<?php
-/************************************************************************/
-  * Responds to the Invitation Page's form action to add a new e-mail
-  * address to our list of subscribed users
-  ************************************************************************/
-<!-- Get the MySQL connection -->
-require_once("mysql_connection.php");
-
-?>
