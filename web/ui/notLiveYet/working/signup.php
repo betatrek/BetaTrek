788b6f930a009ab922703638919b07ff8a762a21
Dandre Allison <dandre.allison@gmail.com>
2012-03-10 15:46:11 -0800

    Debugging PHP

--- web/ui/notLiveYet/working/signup.php
+++ web/ui/notLiveYet/working/signup.php
@@ -92,6 +92,7 @@
 			<!-- For the screen readers -->
 			<p class="visuallyhidden">Algorithmic advice no matter the class.</p>
 				<?php session_start(); ?>
+				<p><?php echo "PHP: "; echo $_SESSION; echo $_POST; ?></p>
 				<?php if ($_SESSION['message']): ?>
 				<p><?php echo $_SESSION['message']; ?></p>
 				<?php endif; ?>
