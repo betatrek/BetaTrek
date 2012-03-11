8371bf0fa464dff3c302e3abfff13223fae62813
Dandre Allison <dandre.allison@gmail.com>
2012-03-10 18:36:34 -0800

    moved PHP code to webpage code

--- web/ui/notLiveYet/working/signup.php
+++ web/ui/notLiveYet/working/signup.php
@@ -27,6 +27,99 @@
     </style>
     <![endif]-->
 <script src="js/libs/modernizr-2.0.6.min.js"></script>
+<?php
+// Get MySQL connection
+include('php/mysql_connection.php');
+// Get Bcrypt one-way encryption algorithm
+include('php/bcrypt.php');
+// Get HTTP requesting
+include('php/load.inc');
+
+$form_button_name = 'create';
+$form_button_value = 'create';
+
+// Prepared statement to verify a email address is available
+$select_statement = $mysql_conn->prepare("SELECT (id) FROM users WHERE email=?") or
+                    die("Prepare failed: (" . $mysql_conn->errno . ") " . $mysql_conn->error);
+$select_statement->bind_param('s', $email);
+// Prepared statement to insert a new user into the database
+$insert_statement = $mysql_conn->prepare("INSERT INTO users (email,password,country,state,id,datestamp) VALUES (?,?,?,?,?,?)") or
+                    die("Prepare failed: (" . $mysql_conn->errno . ") " . $mysql_conn->error);
+$insert_statement->bind_param('ssssss', $email, $password, $country, $state, $id, $datestamp);
+
+if (isAvailable($id)) {
+	// Start a session
+	session_start();
+	// Protext against session fixation attacks
+	session_regenerate_id(true);
+	// set variables in the session
+	$_SESSION['id'] = $id;
+	$_SESSION['HTTP_USER_AGENT'] = $bcrypt->encrypt($_SERVER['HTTP_USER_AGENT']);
+	header('location: portfolio_creation.html');
+} else {
+	//header('location: ../signup.php');
+}
+
+// Close the MySQL connection
+$select_statement->close();
+$insert_statement->close();
+$mysql_conn->close();
+exit;
+
+/**
+ * Confirms that the attempted new account doesn't conflict with an existing account.
+ * @param $id A reference to store the ID of the user upon success to pass to method caller
+ */
+function isAvailable($id) {
+	global $form_button_name, $form_button_value, $insert_statement;
+	$is_available = false;
+	// Make sure POST data is valid
+	if ($_POST[$form_button_name] == $form_button_value) {
+		// Grab the form input data
+		$email = $_POST['email'];
+		$password = $_POST['password'];
+		$country = $_POST['country']; 
+		$state = $_POST['state'];
+		$agree_with_tos = $_POST['agree'];
+		// If user agrees with your Terms of Service
+		if ($agree_with_tos) {
+			// Confirm the email doesn't conflict with that of an existing user
+			if (isInDatabase($id))
+				$_SESSION['message'] = "That email address conflicts with one of an existing user.";
+			else {
+				$datestamp = date('Y-m-d');
+				// id is a secure and unique randomly generated identifier
+				$id = load('http://www.betatrek.com/betatrek/controller/id/getId');
+				$insert_statement->execute();
+				if ($insert_statement->affected_rows > 0) {
+					//TODO: send email confirmation link and update database
+					$is_available = true;
+				} else {
+					$_SESSION['message'] = "Sorry, we incountered an issue creating this new account, " .
+					                       "please try again later.";
+					// TODO: maybe log information about this if it ever occurs?
+				}
+			}
+		// Otherwise tell user to agree before continuing
+		} else {
+			$_SESSION['message'] = "We require that you agree to the Terms of Service before continuing.";
+		}
+	} 
+	
+	return $is_available;
+}
+
+/**
+ * Performs a query to check that a email is in our database.
+ * @param $id A reference to store the ID of the user to pass to the method caller
+ */
+function isInDatabase($id) {
+	global $select_statement;
+	$select_statement->execute();
+	$select_statement->bind_result($id);
+	return $select_statement->fetch();
+}
+?>
 </head>
 
 <body class="signUpPage">
